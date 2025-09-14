085b1c9264acf9b36f6a617fee862a92e49cf9ef
alexmkh <alexmkh33@gmail.com>
2025-09-14 15:47:42 +0000

    Lesson 93

--- backend/listings/api/serializers.py
+++ backend/listings/api/serializers.py
@@ -1,20 +1,24 @@
 from rest_framework import serializers
+from django.contrib.gis.measure import D
+from django.contrib.gis.geos import Point
 
 from listings.models import Listing, Poi
+
 
 class ListingSerializer(serializers.ModelSerializer):
     country = serializers.SerializerMethodField()
     seller_username = serializers.SerializerMethodField()
     seller_agency_name = serializers.SerializerMethodField()
-    listing_pois = serializers.SerializerMethodField()
+    listing_pois_within_10km = serializers.SerializerMethodField()
 
-    def get_listing_pois(self, obj):
-        query = Poi.objects.all()
+    def get_listing_pois_within_10km(self, obj):
+        listing_location = Point(obj.latitude, obj.longitude, srid=4326)
+        query = Poi.objects.filter(location__distance_lte=(listing_location, D(km=10)))
         query_serialized = PoiSerializer(query, many=True)
         return query_serialized.data
 
     def get_seller_agency_name(self, obj):
-        return obj.seller.profile.agency_name if hasattr(obj.seller, 'profile') else None
+        return obj.seller.profile.agency_name
 
     def get_seller_username(self, obj):
         return obj.seller.username
@@ -24,7 +28,7 @@ class ListingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Listing
-        fields = '__all__'
+        fields = "__all__"
 
 
 class PoiSerializer(serializers.ModelSerializer):
@@ -40,4 +44,3 @@ class PoiSerializer(serializers.ModelSerializer):
 
     def get_longitude(self, obj):
         return obj.location.tuple[1] if obj.location else None
-
